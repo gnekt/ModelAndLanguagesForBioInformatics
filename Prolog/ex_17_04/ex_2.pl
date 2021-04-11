212f9a9c0002ff74fbc6be9201e3248104360660
Angel <82314281+angelmpalomares@users.noreply.github.com>
2021-04-11 12:15:28 +0200

    Update ex_2.pl
    Added comand lines 6 and 7. Overlooked cases

--- Prolog/ex_17_04/ex_2.pl
+++ Prolog/ex_17_04/ex_2.pl
@@ -3,3 +3,6 @@ eliminate2consecutivevalues([X,Y|L],X,Y,L):- !.
 eliminate2consecutivevalues([A,B|L],X,Y,[A|Rs]):- A = X, B \= X, eliminate2consecutivevalues([B|L],X,Y,Rs).
 eliminate2consecutivevalues([A,B|L],X,Y,[A|Rs]):- A \= X, B = X, eliminate2consecutivevalues([B|L],X,Y,Rs).
 eliminate2consecutivevalues([A,B|L],X,Y,[A,B|Rs]):- A \= X, B \= X, B \= Y, eliminate2consecutivevalues(L,X,Y,Rs).
+eliminate2consecutivevalues([A,B|L],X,Y,[A,B|RS]):- A= X,B \= Y,B \= X, eliminate2consecutivevalues(L,X,Y,RS).
+eliminate2consecutivevalues([A,B|L],X,Y,[A|RS]):-A = X,B \= Y,B = X, eliminate2consecutivevalues([B|L],X,Y,RS).
+                            
