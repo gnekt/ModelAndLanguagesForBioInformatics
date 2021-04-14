a1ad39a2af4a4a70a13e9ac912bd814b1ee40e77
Christian <c.dimaio1@student.unisi.it>
2021-04-14 16:40:34 +0200

    new ex. added

--- Prolog/ex_17_04/ex_2.pl
+++ Prolog/ex_17_04/ex_2.pl
@@ -6,3 +6,9 @@ eliminate2consecutivevalues([A,B|L],X,Y,[A,B|Rs]):- A \= X, B \= X, B \= Y, elim
 eliminate2consecutivevalues([A,B|L],X,Y,[A,B|RS]):- A= X,B \= Y,B \= X, eliminate2consecutivevalues(L,X,Y,RS).
 eliminate2consecutivevalues([A,B|L],X,Y,[A|RS]):-A = X,B \= Y,B = X, eliminate2consecutivevalues([B|L],X,Y,RS).
                             
+% Soluzione prof
+
+eliminate2([],[]).
+eliminate2([X],X,Y,[X]).
+eliminate2([X,Y|L],X,Y,L).
+eliminate2([X1,Y1|L],X,Y,[X1|LO]):- [X1,Y1] \== [X,Y], eliminate2([Y1|L],X,Y,LO).
