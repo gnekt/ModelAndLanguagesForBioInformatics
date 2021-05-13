ad5f29f509fae51209c6ebd4392b944f8b2fefab
Christian Di Maio <dima9610@gmail.com>
2021-05-14 01:41:46 +0200

    Added exercises

---
+++ Python/List/15.permutation.py
@@ -0,0 +1,31 @@
+import random
+
+
+def one_permutation(in_list1: list, ver) -> list:
+    """
+    Perform a permutation of the input list
+    :param in_list1: the source list
+    :return: a permutation of the list
+    """
+    if ver == 0:
+        return random.shuffle(in_list1)
+    if ver == 1:
+        _shallow_list = in_list1.copy()
+        _shuffled_list = []
+        while len(_shallow_list) != 0:
+            _index = random.randint(0,len(_shallow_list)-1)
+            element = _shallow_list[_index]
+            _shuffled_list.append(element)
+            _shallow_list.remove(element)
+        return _shuffled_list
+
+def all_permutations(start: list, end=[]):
+    if len(start) == 0:
+        print(end)
+    else:
+        for i in range(len(start)):
+            all_permutations(start[:i] + start[i+1:], end + start[i:i+1])
+
+
+if __name__ == "__main__":
+    all_permutations([1,2,3,4])
---
+++ Python/List/16.mergesort.py
@@ -0,0 +1,39 @@
+def merge_ordered_list(in_list1: list, in_list2: list) -> list:
+    """
+    Merge two ordered list
+    :param in_list1: the first source list
+    :param in_list2: the second source list
+    :return: the merged list
+    """
+    _list1 = in_list1.copy()
+    _list2 = in_list2.copy()
+    _output_list = []
+    idx_2 = 0
+    for element in _list1:
+        while idx_2 < len(_list2) and element > _list2[idx_2]:
+            _output_list.append(_list2[idx_2])
+            idx_2 += 1
+        _output_list.append(element)
+    while idx_2 < len(_list2):
+        _output_list.append(_list2[idx_2])
+        idx_2 += 1
+    return _output_list
+
+
+def merge_sort(in_list1: list) -> list:
+    """
+    Do the merge sort for a given list in input
+    :param in_list1: the list to be ordered
+    :return: the ordered list
+    """
+    if in_list1 is None:
+        return []
+    if len(in_list1) == 1:
+        return [in_list1[0]]
+    _list1,_list2= in_list1[:int(((len(in_list1)+1)/2))],in_list1[int(((len(in_list1)+1)/2)):]
+    _ordered_list1 = merge_sort(_list1)
+    _ordered_list2 = merge_sort(_list2)
+    return merge_ordered_list(_ordered_list1,_ordered_list2)
+
+if __name__ == "__main__":
+    print(merge_sort([1,20,3,32,4,9,9,10]))
