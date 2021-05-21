872fccbccaa8e1d813321f185b466cc824e0ea99
Christian Di Maio <dima9610@gmail.com>
2021-05-21 19:49:16 +0200

    Update 16.Diameter.py
    In this file there are also other exercises to be tested

--- Python/Tree/16.Diameter.py
+++ Python/Tree/16.Diameter.py
@@ -36,5 +36,122 @@ def diameter(tree: Tree) -> int:
     diameter_r = diameter(tree.right)
     return max(heigth_r+heigth_l+1, max(diameter_r,diameter_l))
 
+def sum_leaves(tree: Tree) -> int:
+    if tree:
+        if not tree.right and not tree.left:
+            return tree.elem
+        return sum_leaves(tree.left) + sum_leaves(tree.right)
+    return 0
+
+def leaves_to_list(tree: Tree) -> int:
+    if tree:
+        if not tree.right and not tree.left:
+            return [tree.elem]
+        return leaves_to_list(tree.left)+leaves_to_list(tree.right)
+    return []
+
+def internal_to_list(tree: Tree) -> int:
+    if tree:
+        if not tree.right and not tree.left:
+            return []
+        return [tree.elem]+internal_to_list(tree.left)+internal_to_list(tree.right)
+    return []
+
+def count_occurrence(tree: Tree, node: int) -> int:
+    if tree is None: return 0
+    counter = 0
+    if tree.elem == node:
+        counter = 1
+    if tree.left:
+        counter += count_occurrence(tree.left,node)
+    if tree.right:
+        counter += count_occurrence(tree.right, node)
+    return counter
+
+def count_occurrence_bst(tree: Tree, node: int) -> int:
+    if tree is None: return 0
+    counter = 0
+    if tree.elem == node:
+        counter = 1
+    if tree.left and tree.elem > node:
+        counter += count_occurrence(tree.left,node)
+    elif tree.right:
+        counter += count_occurrence(tree.right, node)
+    return counter
+
+def tree_is_subtree(tree: Tree, sub_tree: Tree) -> bool:
+    """
+    Check if a given tree(sub_tree) is a sub-tree of a tree(tree), it may be robust to
+    multiple occurrence of the root node
+    :param tree: The Target tree where we want to look
+    :param sub_tree: The source tree
+    :return: True if the statement hold, otherwise False
+    """
+    if tree and not sub_tree: return True
+    if not tree and not sub_tree: return True
+    if not tree and sub_tree: return False
+    equal_l, equal_r = False, False
+    if sub_tree.elem == tree.elem:
+        equal_l = tree_is_subtree(tree.left,sub_tree.left)
+        equal_r = tree_is_subtree(tree.right,sub_tree.right)
+    if equal_l and equal_r:
+        return True
+    else:
+        check_l = tree_is_subtree(tree.left, sub_tree)
+        check_r = tree_is_subtree(tree.right, sub_tree)
+        return check_r or check_l
+
+def position_value_intersection(tree1: Tree, tree2: Tree) -> list:
+    if not tree1 or not tree2:
+        return []
+    if tree1.elem == tree2.elem:
+        return [tree1.elem]+position_value_intersection(tree1.left,tree2.left)+ \
+                    position_value_intersection(tree1.right, tree2.right)
+    return position_value_intersection(tree1.left,tree2.left)+ \
+                    position_value_intersection(tree1.right, tree2.right)
+
+def symmetric_trees(tree1: Tree, tree2: Tree) -> bool:
+    if tree1 and tree2:
+        if tree1.elem == tree2.elem:
+            return True
+        return symmetric_trees(tree1.left,tree2.right) and symmetric_trees(tree1.right,tree2.left)
+    return False
+
+def sum_all_nodes(i_tree: Tree) -> float:
+    if i_tree:
+        return i_tree.elem + sum_all_nodes(i_tree.left) + sum_all_nodes(i_tree.right)
+    return 0
+
+def path_bst(i_tree: Tree, node: int) -> list:
+    """
+    Path finder starting from the root node, the last element of the output list
+                                    will be the target node of the finder
+    :param i_tree:
+    :param node:
+    :param temp_path:
+    :return:
+    """
+    if i_tree:
+        if i_tree.elem == node: return [node]
+        if i_tree.elem > node:
+            return [i_tree.elem] + path_bst(i_tree.left,node)
+        else:
+            return [i_tree.elem] + path_bst(i_tree.right, node)
+    return []
+
+def path(i_tree: Tree, node: int) -> list:
+    if i_tree:
+        if i_tree.elem == node: return [node]
+        path_s, path_r = [],[]
+        path_s = [i_tree.elem] + path(i_tree.left,node)
+        path_r = [i_tree.elem] + path(i_tree.right,node)
+        if node in path_s:
+            return path_s
+        if node in path_r:
+            return path_r
+    return []
+
 if __name__ == "__main__":
-    print(diameter(Tree(1,Tree(1,Tree(1,Tree(1),Tree(1))),Tree(1))))
+    print(symmetric_trees(Tree(2,Tree(1),Tree(2,Tree(1,None,Tree(2)))),Tree(2,Tree(2,None,Tree(1,Tree(2))),Tree(1))))
+    print(sum_all_nodes(Tree(2,Tree(1),Tree(2,Tree(1,None,Tree(2))))))
+    print(path(Tree(3, Tree(1,None,Tree(6)), Tree(6, Tree(4), Tree(7,None,Tree(9)))),6))
